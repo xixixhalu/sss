483e6e3a55e72a11f8e877f5f8b315dbc9e37c29
bohan2345 <bohan.zheng@gmail.com>
2014-01-17 14:42:12 -0800

    asdfghnm,.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -7,6 +7,7 @@ import views.html.*;
 public class Application extends Controller {
 
     public static Result index() {
+    ////////////大大大大是
         //return ok(index.render("Your new application is ready."));
     	return redirect(routes.CourseController.retrieveCourses());
     }
