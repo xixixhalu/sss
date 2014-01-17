c1c91d660ba128cde3e8d74eab3cdc4bbf037f73
rtong <rtong@usc.edu>
2014-01-16 22:01:56 -0800

    wrong again and again
    wrong again and again

--- app/controllers/CourseController.java
+++ app/controllers/CourseController.java
@@ -28,7 +28,7 @@ public class CourseController extends Controller {
     	Form<CourseEditForm> filledForm = Form.form(CourseEditForm.class).bindFromRequest();
     	
     	if(filledForm.hasErrors()) {
-    		return badRequest("Wrong agian");
+    		return badRequest("Wrong agian and agian");
     	} 
     	else {
     		CourseEditForm courseForm = filledForm.get();
