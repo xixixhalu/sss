f869ef63af8cfb38deac4193e456763999521ae7
bohan2345 <bohan.zheng@gmail.com>
2014-02-21 17:45:26 -0800

    simplerequirement

--- public/javascripts/add_s_req.js
+++ public/javascripts/add_s_req.js
@@ -9,10 +9,12 @@ function addSimpleReq() {
     var li = document.createElement("li");
     li.innerHTML = "<a onclick='deleteSimpleReq(this)'></a>";
     if (simpleReqList.children.length > 1) {
-        li.innerHTML += "<select name=" + simpleReq.value + "><option value='and'>AND</option><option value='or'>OR</option><option value='not'>NOT</option></select>";
+        li.innerHTML += "<select name='simpleReqRel'><option value='and'>AND</option><option value='or'>OR</option><option value='not'>NOT</option></select>";
     } else
-        li.innerHTML += "<span style='width:29px;border:white;'>&nbsp;</span>";
-    li.innerHTML += "<span onclick='changePriority(this)'>" + simpleReqTitle + "</span>";
+        li.innerHTML += "<span name='simpleReqRel' style='width:29px;border:white;' value=''>&nbsp;</span>";
+    li.innerHTML += "<input type='hidden' name='simpleReqId' value=" + simpleReq.value + ">";
+    li.innerHTML += "<span>" + simpleReqTitle + "</span>";
+    li.innerHTML += "<input type='text' name='simpleReqGroup' value='1'>";
     simpleReqList.appendChild(li);
 }
 
@@ -25,12 +27,15 @@ function deleteSimpleReq(node) {
     if (liList.length > 0) {
         var fli = liList[0];
         var fselect = fli.children[1];
-        var ftitle = fli.children[2];
+        var ftitle = fli.children[3];
+        var fgroup=fli.children[4];
         var fspan = generateSpace();
         fli.removeChild(fselect);
         fli.removeChild(ftitle);
+        fli.removeChild(fgroup);
         fli.appendChild(fspan);
         fli.appendChild(ftitle);
+        fli.appendChild(fgroup);
     }
 }
 
@@ -60,13 +65,26 @@ function getSelectedText(obj) {
     }
 }
 
-function generateExpression(simpleReqObjs) {
-    var expression = "";
+//define a course object
+function courseObj(cId, cName, cRelation, cGroup) {
+    this.id = cId;
+    this.name = cName;
+    this.relation = cRelation;
+    this.group = cGroup;
+}
+
+function generateJsonObj(simpleReqObjs) {
+    var temp = new Array;
     for ( i = 0; i < simpleReqObjs.length; i++) {
-        expression += simpleReqObjs[i].getElementsByTagName("select")[0].name;
-        expression += simpleReqObjs[i].getElementsByTagName("select")[0].value;
+        //create a course object
+        var id = document.getElementsByName("simpleReqId")[i].value;
+        var name = simpleReqObjs[i].Children[3].innerHTML;
+        var relation = document.getElementsByName("simpleReqRel")[i].value;
+        var group = document.getElementsByName("simpleReqGroup")[i].value;
+        var req = new courseObj(id, name, relation, group);
+        temp.push(req);
     }
-    return expression;
+    return temp;
 }
 
 function doSubmit() {
@@ -74,7 +92,7 @@ function doSubmit() {
     hidden.type = "hidden";
     var simpleReqObjs = new Array;
     simpleReqObjs = document.getElementById("s_list").getElementsByTagName("li");
-    hidden.value = generateExpression(simpleReqObjs);
+    hidden.value = generateJsonObj(simpleReqObjs);
     hidden.name = "sr_ids";
     document.getElementById("s_submit").appendChild(hidden);
 }
@@ -84,17 +102,22 @@ function init(simpleReqs) {
         "id" : "1",
         "name" : "simple requirement 1",
         "relation" : "",
-        "priority" : "1"
+        "group" : "1"
     }, {
         "id" : "2",
         "name" : "simple requirement 2",
         "relation" : "and",
-        "priority" : "1"
+        "group" : "1"
     }, {
         "id" : "3",
         "name" : "simple requirement 3",
+        "relation" : "or",
+        "group" : "2"
+    }, {
+        "id" : "4",
+        "name" : "simple requirement 4",
         "relation" : "and",
-        "priority" : "1"
+        "group" : "2"
     }];
 
     //var simpleReqs = eval("(" + simpleReqs + ")");
@@ -102,7 +125,13 @@ function init(simpleReqs) {
     var simpleReqList = document.getElementById("s_list");
     for ( i = 0; i < simpleReqs.length; i++) {
         var li = document.createElement("li");
-
+        
+        var hidden = document.createElement("input");
+        hidden.value = simpleReqs[i].id;
+        hidden.type = "hidden";
+        hidden.name = "simpleReqId";
+        li.appendChild(hidden);
+        
         var button = document.createElement("a");
         button.onclick = function() {
             deleteSimpleReq(this);
@@ -113,9 +142,8 @@ function init(simpleReqs) {
             var space = generateSpace();
             li.appendChild(space);
         } else {
-
             var select = document.createElement("select");
-            select.name = simpleReqs[i].id;
+            select.name = "simpleReqRel";
 
             var and = document.createElement("option");
             if (simpleReqs[i].relation == "and")
@@ -135,13 +163,20 @@ function init(simpleReqs) {
             select.appendChild(not);
             li.appendChild(select);
         }
+        
         var span = document.createElement("span");
         span.innerHTML = simpleReqs[i].name;
-        span.onclick=function(){
+        span.onclick = function() {
             changePriority(this);
         };
         li.appendChild(span);
-
+        
+        var text=document.createElement("input");
+        text.type="text";
+        text.value=simpleReqs[i].group;
+        text.name="simpleReqGroup";
+        li.appendChild(text);
+        //添加组
         simpleReqList.appendChild(li);
     }
 }
