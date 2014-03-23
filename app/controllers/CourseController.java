7af4b9ef6e8cbb423e14608ece6e7d76f520fb5d
uniquews <wstjpu@gmail.com>
2014-03-23 16:11:47 -0700

    auto

--- app/controllers/CourseController.java
+++ app/controllers/CourseController.java
@@ -15,13 +15,13 @@ import java.util.List;
 public class CourseController extends Controller {
 
     public static Result retrieveCourses() {
-    	try{
+    	//try{
     		StudyPlanController.CreateDegreeProgram(new Integer(5));
     		return ok(views.html.course_list.render(Course.getAll()));
-    	}catch(Exception e)
-    	{
-    		return badRequest(views.html.error.render("Cannot retrieve course list"));
-    	}
+    	//}catch(Exception e)
+    	//{
+    	//	return badRequest(views.html.error.render("Cannot retrieve course list: " + e.toString()));
+    	//}
     }
    
     public static Result deleteCourse(Integer id){
